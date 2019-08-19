27c6cc4f956480ed560b42e9224fe5c3ce502d56
GabrielPanta <gabrielpantajimenez@hotmail.com>
2019-08-19 17:32:09 -0500

    LOGGING & SERILOG TO FILESYSTEM

---
+++ kodotiUser/src/KODOTIFront/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using KODOTIFront.Models;
+using Microsoft.Extensions.Logging;
+
+namespace KODOTIFront.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Runnig HomeController:Index...");
+            return View();
+        }
+
+        public IActionResult Error2()
+        {
+            throw new System.Exception("UPPSS ERROR MOTHERFCUKE!!!");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
