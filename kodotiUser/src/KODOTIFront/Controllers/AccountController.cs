f2cbdf06330686069797277c395f70667343d54d
GabrielPanta <gabrielpantajimenez@hotmail.com>
2019-08-20 10:45:57 -0500

    ASP.NET IDENTITY CORE: LOGIN

--- kodotiUser/src/KODOTIFront/Controllers/AccountController.cs
+++ kodotiUser/src/KODOTIFront/Controllers/AccountController.cs
@@ -12,10 +12,31 @@ namespace KODOTIFront.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(UserLoginViewModel model)
+        {
+            var result=await _signInManager.PasswordSignInAsync(
+                model.Email, 
+                model.Password,
+                model.RememberMe, 
+                false);
+            if (result.Succeeded)
+            {
+                return Redirect("~/");
+            }
+            return View();
         }
         public IActionResult NewUser()
         {
@@ -29,9 +50,14 @@ namespace KODOTIFront.Controllers
             {
                 UserName=model.Email,
                 Email = model.Email,
-                
             },model.Password) ;
             return View(result.Succeeded);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return Redirect("~/");
         }
     }
 }
